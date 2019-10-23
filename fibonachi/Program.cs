c12fee1c972bef9857ed401e2bd8874bd7556408
divergent3 <sergejsabala@gmail.com>
2019-10-24 01:28:25 +0300

    Добавьте файлы проекта.

---
+++ fibonachi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonachi {
+    class Program {
+        static void Main(string[] args) {
+        int a = 1, b = 0, c = 1;
+       int length = 1000;
+        for(int i = 0; i < length;) {
+        c = a + b;
+        a = b;
+        b = c;
+        Console.WriteLine(c);
+        Console.ReadLine();
+        }
+        }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
